be3f66be204c4ca2d39e5236833621a24ce779d8
Miguel Ángel Botija Ortega <miguelangel.botija@gmail.com>
2020-07-07 20:33:38 +0200

    Adds an interceptor to log network requests.

--- app/src/main/java/com/miguelangelboti/stocks/data/network/NetworkDataSource.kt
+++ app/src/main/java/com/miguelangelboti/stocks/data/network/NetworkDataSource.kt
@@ -4,15 +4,19 @@ import com.miguelangelboti.stocks.data.entities.StockInfo
 import com.miguelangelboti.stocks.data.network.alphavantage.AlphaVantageApi
 import com.miguelangelboti.stocks.data.network.alphavantage.results.toDomain
 import com.miguelangelboti.stocks.entities.Stock
+import okhttp3.logging.HttpLoggingInterceptor
+import okhttp3.logging.HttpLoggingInterceptor.Level.BODY
 import retrofit2.Retrofit.Builder
 import retrofit2.converter.moshi.MoshiConverterFactory
 import timber.log.Timber
+import okhttp3.OkHttpClient.Builder as OkHttpBuilder
 
 class NetworkDataSource {
 
     private var service = Builder()
         .baseUrl("https://www.alphavantage.co/")
         .addConverterFactory(MoshiConverterFactory.create())
+        .client(OkHttpBuilder().addInterceptor(HttpLoggingInterceptor().apply { level = BODY }).build())
         .build()
         .create(AlphaVantageApi::class.java)
 
